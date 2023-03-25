3f336d6cd8492705954aad1c706fc251973b6611
Gaziz <maks.mafia00@mail.ru>
2023-03-25 20:21:48 +0600

    build: assets

---
+++ src/shared/assets/image/index.ts
@@ -0,0 +1,4 @@
+export { default as Logo } from './logo.png'
+export { default as MiniLogo } from './mini-logo.png'
+export { default as Hero } from './hero.png'
+export { default as Hero2 } from './hero2.png'
