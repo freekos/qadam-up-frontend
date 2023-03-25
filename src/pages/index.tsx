4dcc0fe86b088d81cd5b4fe580b1ea984b8249c6
Gaziz <maks.mafia00@mail.ru>
2023-03-25 21:07:05 +0600

    refactor: routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router'
 
+import { lazy } from '~src/shared/lib'
 import { paths, routes } from '~src/shared/routes'
 
-// const LandingPage =
+const LandingPage = lazy(() => import('./landing'), 'LandingPage')
+const HomePage = lazy(() => import('./home'), 'HomePage')
+const Error404Page = lazy(() => import('./error-404'), 'Error404Page')
 
 export function Pages() {
 	return (
 		<Routes>
 			<Route path={paths.landing} element={<LandingPage />} />
+			<Route path={paths.home} element={<HomePage />} />
+			<Route path='*' element={<Error404Page />} />
 		</Routes>
 	)
 }
