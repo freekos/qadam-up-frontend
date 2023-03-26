042175bd905ec2df2cfb12e6cc3b23703ca1af2d
Gaziz <maks.mafia00@mail.ru>
2023-03-26 09:44:29 +0600

    refactor: routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,17 @@ import { paths, routes } from '~src/shared/routes'
 
 const LandingPage = lazy(() => import('./landing'), 'LandingPage')
 const HomePage = lazy(() => import('./home'), 'HomePage')
-const Error404Page = lazy(() => import('./error-404'), 'Error404Page')
+const SignUpPage = lazy(() => import('./sign_up'), 'SignUpPage')
+const SignInPage = lazy(() => import('./sign_in'), 'SignInPage')
+const Error404Page = lazy(() => import('./error_404'), 'Error404Page')
 
 export function Pages() {
 	return (
 		<Routes>
 			<Route path={paths.landing} element={<LandingPage />} />
 			<Route path={paths.home} element={<HomePage />} />
+			<Route path={paths.signUp} element={<SignUpPage />} />
+			<Route path={paths.signIn} element={<SignInPage />} />
 			<Route path='*' element={<Error404Page />} />
 		</Routes>
 	)
--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,9 +1,17 @@
 export const paths = {
-	landing: '/about',
-	home: '/',
+	landing: '/',
+	home: '/home',
+	signIn: '/sign_in',
+	signUp: '/sign_up',
+	aboutUs: '/about_us',
+	mentors: '/mentors',
 }
 
 export const routes = {
 	landing: () => paths.landing,
 	home: () => paths.home,
+	signIn: () => paths.signIn,
+	signUp: () => paths.signUp,
+	aboutUs: () => paths.aboutUs,
+	mentors: () => paths.mentors,
 }
