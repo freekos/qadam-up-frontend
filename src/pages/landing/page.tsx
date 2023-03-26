ba242be2a679ff1c9ebe76cda9c8004e655fd4bf
Gaziz <maks.mafia00@mail.ru>
2023-03-26 10:03:28 +0600

    refactor: pages/landing

--- src/pages/landing/page.tsx
+++ src/pages/landing/page.tsx
@@ -1,3 +1,33 @@
+import { Button, Layout, Typography } from 'antd'
+import cn from 'classnames'
+
+import styles from './styles.module.scss'
+
+const heroTitle = `Welcome to QadamUp`
+const heroText = `This website offers a comprehensive platform for individuals who aspire to achieve professional excellence in their respective fields. Whether you're starting out in your career or looking to advance to the next level, we provide the resources and tools necessary to help you succeed.`
+
 export function LandingPage() {
-	return <></>
+	return (
+		<Layout className={styles.root}>
+			<HeroSection />
+		</Layout>
+	)
+}
+
+function HeroSection() {
+	return (
+		<Layout.Content className={styles.hero_section}>
+			<div className={styles.hero_content}>
+				<div className={styles.hero_text_container}>
+					<Typography.Title>{heroTitle}</Typography.Title>
+					<Typography.Title>{heroText}</Typography.Title>
+
+					<Button type='primary' size='large'>
+						GETTING STARTED
+					</Button>
+				</div>
+			</div>
+			<div className={styles.hero_image} />
+		</Layout.Content>
+	)
 }
